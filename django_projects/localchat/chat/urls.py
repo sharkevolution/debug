eea32b10b50165bf0e788de9871902abe1224566
Sitala <nsitala@ukr.net>
2023-03-20 15:57:45 +0200

    up

--- django_projects/localchat/chat/urls.py
+++ django_projects/localchat/chat/urls.py
@@ -12,11 +12,25 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
 
     path('password-change/',
-         auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'),
+         auth_views.PasswordChangeView.as_view(
+             template_name='password_change_form.html'),
          name='password_change'),
 
     path('password-change/done/',
          auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
+
+    # reset password urls
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
